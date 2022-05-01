3005e34fd558f7e9ceecf36e9635b509a8604526
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-02 00:56:59 +0600

    register component added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = event => {
+        navigate("/login")
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>Place Register</h2>
+        <div className='register-form container'>
+            <h2 className='text-center text-primary'>Place Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name'required/>
+                <input type="email" name="email" id="" placeholder="Email" required/>
+                <input type="password" name="password" id="" placeholder="Password" required />
+                <input type="submit" value="Register" required />
+            </form>
+            <p>Already have account ?<Link to="/login" className='text-danger pe-auto text-decoration-none ' onClick={navigateLogin}> Please Login</Link> </p>
         </div>
     );
 };
